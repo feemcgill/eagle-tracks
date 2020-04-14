9a3a3d1e1493321f294e9d77f17f300455c93b52
Brendan Sheehan <feemcgill@gmail.com>
2020-04-14 02:11:53 -0700

    finally add google anal

--- src/App.vue
+++ src/App.vue
@@ -7,14 +7,6 @@
     <router-view/>
   </div>
 </template>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-158456330-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'UA-158456330-1');
-</script>
 
 <style>
 html, body, div, span, applet, object, iframe,
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueGtag from "vue-gtag";
+
+Vue.use(VueGtag, {
+  config: { id: "UA-158456330-1" }
+});
 
 Vue.config.productionTip = false
 
