c3676319a56eadceb16c478efbbf6cf82be87cc0
Brendan Sheehan <brendan@adp.bz>
2021-09-28 14:36:53 -0700

    adding ga and back button

--- src/main.js
+++ src/main.js
@@ -1,33 +1,29 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from "vue";
+import VueRouter from "vue-router";
 import VueGtag from "vue-gtag";
-import App from './App.vue'
-import Home from './views/Home.vue'
-import Square from './views/Square.vue'
+import App from "./App.vue";
+import Home from "./views/Home.vue";
+import Square from "./views/Square.vue";
 
+// Vue.use(VueGtag, {
+//   config: { id: "UA-158456330-1" }
+// });
 
-Vue.use(VueGtag, {
-  config: { id: "UA-158456330-1" }
-});
+Vue.use(VueRouter);
 
-Vue.use(VueRouter)
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/square', component: Square }
-]
-
+  { path: "/", component: Home },
+  { path: "/square", component: Square }
+];
 
 const router = new VueRouter({
   routes,
-  mode: 'history'
+  mode: "history"
 });
-
-
 
 new Vue({
   render: h => h(App),
-  router: router,
-}).$mount('#app')
+  router: router
+}).$mount("#app");
